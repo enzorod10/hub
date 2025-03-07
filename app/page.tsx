0eb7ed3f531ce489cf52a9d2e9fa8e8794dcd0a2
enzo <me@enzorod.com>
2025-03-06 23:34:51 -0500

    added shadcn and labels for tasks

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@
 import { useState } from "react";
 import { Task } from "./types";
 import { v4 as uuidv4 } from 'uuid';
+import { Label } from "@/components/ui/label"
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
 
 export default function Home() {
   const [tasks, setTasks] = useState<Task[]>();
@@ -33,6 +35,20 @@ export default function Home() {
           <div className="w-full border relative rounded-md p-4 gap-4 justify-center">
             <form>
               <input onChange={(e) => setTask({...task, name: e.target.value})} type="text" placeholder="Add a task" className="w-full border rounded-md p-4" />
+              <RadioGroup defaultValue="comfortable">
+                <div className="flex items-center space-x-2">
+                  <RadioGroupItem value="default" id="r1" />
+                  <Label htmlFor="r1">Low Prio</Label>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <RadioGroupItem value="comfortable" id="r2" />
+                  <Label htmlFor="r2">Medium Prio</Label>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <RadioGroupItem value="compact" id="r3" />
+                  <Label htmlFor="r3">High Prio</Label>
+                </div>
+              </RadioGroup>
               <button onClick={(e) => handleAddTask(e)} className="text-2xl absolute left-0 top-0">
                 +
               </button>
