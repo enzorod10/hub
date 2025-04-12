fe54ec32e2ae33456cefff8d9bdebdfff46c1433
enzo <me@enzorod.com>
2025-04-11 22:46:22 -0400

    made tasks server side using supabase db

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,7 @@ import { UserNav } from './user-nav';
 
 export default function Header() {
     return (
-      <div className="fixed top-0 left-0 right-0 supports-backdrop-blur:bg-background/60 border-b bg-background/95 backdrop-blur z-20">
+      <div className="top-0 left-0 right-0 supports-backdrop-blur:bg-background/60 border-b bg-background/95 backdrop-blur z-20">
         <nav className="h-14 flex items-center justify-between px-4">
           <div className='flex items-center gap-3'>
                 <Avatar className="h-9 w-9">
@@ -18,7 +18,7 @@ export default function Header() {
                 </h4>
           </div>
           <div className="flex items-center gap-2">
-            {/* <UserNav /> */}
+            <UserNav />
             {/* <ThemeToggle /> */}
           </div>
         </nav>
